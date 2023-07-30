e966d370bf3082fb99004d2a5ed93b8b80b7bede
fer5899 <fdometallic@gmail.com>
2023-07-30 14:55:50 +0200

    Cosa

--- Yate-lo-dije/Assets/Scripts/GameManager.cs
+++ Yate-lo-dije/Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public int score;
+    public GameObject scoreUI;
+    public int score = 15;
     public int destroyedFragatas;
     public int destroyedAcorazados;
     public int destroyedHelicopteros;
@@ -13,12 +16,15 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(scoreUI.GetComponent<TextMeshProUGUI>().text);
+        scoreUI.GetComponent<TextMeshProUGUI>().SetText(score.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+            SceneManager.LoadScene(0);
+        //if ()
     }
 }
